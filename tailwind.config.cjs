df88a11065458e7bd6c3076d8822a844db0c8571
Alex Werner <alewern@pm.me>
2022-04-20 08:17:48 -0500

    Updates theme in tailwind.config.cjs and checks that theme is available in index.astro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,35 @@ module.exports = {
 	mode: 'jit',
 	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+		},
+		colors: {
+			yellow: '#F8D948',
+			green: '#48F881',
+			pink: '#F848BF',
+			blue: '#4867F8',
+			white: '#FFFFFF',
+			black: '#282828',
+			gray: {
+				100: '#939597',
+				200: '#797B7E'
+			}
+		},
+		fontFamily: {
+			sans: ['Gobold High', 'sans-serif'],
+		},
+		borderRadius: {
+			'none': '0',
+      		'sm': '.125rem',
+      		DEFAULT: '.25rem',
+      		'lg': '.5rem',
+      		'full': '9999px',
+		},
 	},
 	plugins: [],
 }
